a7f0f829f42b068b8f36b1b7ee114cb108aa5965
J3eff <jeffersonvideo125@gmail.com>
2022-04-23 17:25:16 -0300

    First commit

---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2022,04, 25);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(60); //dataFimPagamento - dataCorrente
+
+            Console.WriteLine("Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca));
+
+            Console.ReadLine();
+        }
+       
+    }
+}
